cc69cc0317abe4916d6604682c3c3850f473a61e
Hoitink <l.d.hoitink@uu.nl>
2024-02-23 10:13:34 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "index.html",
+    "src/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "orange": "#eb8d59",
+      }
+    },
+  },
+  plugins: [],
+}
+
